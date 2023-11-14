3482fd59f88c1fb7b568b51c3bd32c87fbd6a624
Atharva Jaurkar <atharvajaurkar.2003@gmail.com>
2023-11-14 13:16:58 +0530

    some editing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 import Header from "../components/Header";
+import design1Image from '../assets/design1.jpeg';
+
 
 function Home() {
     return(
         <>
         <Header/>
+        
+        <div className="mega">
+        <div className="box1">
         <div className="combinedSection">
             <section className="heading">
-                <h1>What do you need help with?</h1>
+                <h1>Help Needed ?</h1>
                 <p>Please choose from an option below</p>
             </section>
             <Link to='/new-ticket' className="btn btn-reverse btn-block">
@@ -17,7 +22,16 @@ function Home() {
             <Link to='/tickets' className="btn btn-block">
                 <FaTicketAlt /> View My Tickets
             </Link>
+        
+        </div></div>
+       <div className="box2">
+        <div className="homeImage">
+        <img src={design1Image} alt="HOME IMAGE" />
+        </div></div>
+
         </div>
+      
+      
         </>
     )
 }
